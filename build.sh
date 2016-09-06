99450df66554e3dbd098c35236351fad8ac7e797
huming <mx.mean@gmail.com>
2016-09-06 14:08:42 +0800

    abc

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-
-echo $LANG
-echo $JAVA_HOME
-
-
+function check() {
+  if [ $1 != 0 ];then
+    echo "exec fail"
+    exit 1
+  fi
+}
 source /etc/profile
-
-echo "java ---"
-echo $JAVA_HOME
-/usr/java/jdk1.8.0_91/java -version
-echo "version---"
+java -version
+PROJECT_NAME=mw_auto
+gradle clean && gradle installDist -x Test
+check $?
+cp -r conf/ build/install/"$PROJECT_NAME"/bin/conf/
+cd build/install/
+tar cvzf  "$PROJECT_NAME".tar.gz "$PROJECT_NAME"/
+mv -f "$PROJECT_NAME".tar.gz ../../
+echo "MW_SUCCESS"
